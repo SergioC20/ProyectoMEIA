105e186eba6c3e7bb67729f970aa430d1eb4ca06
Alejandro Gil <91078699+ApothiGoat@users.noreply.github.com>
2024-10-16 00:25:25 -0600

    Verificacion Usuario
    Se agrego la verificacion para confirmar si el usuario ya existe en el archivo

--- GestionDeArchivos/obj/Debug/net8.0/GestionDeArchivos.AssemblyInfo.cs
+++ GestionDeArchivos/obj/Debug/net8.0/GestionDeArchivos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionDeArchivos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a60d63fbac14178d9fb8c1392a2053375938e38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96aaa20dc0e3ae54b10c7bb78d213e487a128bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionDeArchivos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionDeArchivos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
